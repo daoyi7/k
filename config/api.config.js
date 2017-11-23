9fcc86ebe49b66bc0d0a0b7e1878dc8db86fbd4f
daoyi7 <kawhichina@gmail.com>
2017-11-23 23:21:31 +0800

    complete

--- config/api.config.js
+++ config/api.config.js
@@ -1,5 +1,5 @@
 const isPro = Object.is(process.env.NODE_ENV, 'production')
 
 module.exports = {
-  baseURL: isPro ? 'http://47.96.30.198/' :'api/'
+  baseURL: isPro ? 'http://47.96.30.198//api/' :'api/'
 }
